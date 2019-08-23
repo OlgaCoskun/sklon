4b5a34e009bce972eeab9f679ab201fae2ff72b4
olga coskun <ovin999@gmail.com>
2019-08-23 16:56:54 +0300

    Those tests no need anymore

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -7,20 +7,6 @@ RSpec.describe Person, type: :model do
     it { should validate_presence_of :first_name }
   end
 
-  describe '#my_firstname, #my_lastname, #my_middlename' do
-    it 'should return first name' do
-      expect(person.my_firstname).to eq 'Ольга'
-    end
-
-    it 'should return last name' do
-      expect(person.my_lastname).to eq 'Виноградова'
-    end
-
-    it 'should return middle name' do
-      expect(person.my_middlename).to eq 'Александровна'
-    end
-  end
-
   context 'check full_name' do
     it 'should return full_name' do
       expect(person.create_full_name).to eq 'Виноградова Ольга Александровна'
